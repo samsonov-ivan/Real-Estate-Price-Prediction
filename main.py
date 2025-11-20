9d33c2d3f14c988c4b9af11b7174b8094aa500db
ivan.samsonov <samsonov.ivan@phystech.edu>
2025-11-20 13:24:24 +0300

    create main class

---
+++ main.py
@@ -0,0 +1,38 @@
+import pandas as pd
+import numpy as np
+from src.preprocessor import CustomPreprocessor
+from src.trainer import ModelTrainer
+
+
+def load_data(file_path = None):
+    """
+    Load dataset from a CSV file.
+    """
+
+    if file_path is None:
+        # Default dataset path
+        raise ValueError("No file path provided for dataset.")
+    
+    return pd.read_csv(file_path)
+
+
+def main():
+    data = load_data("data/raw.csv")
+    
+    target_column = 'price'
+    
+    preprocessor = CustomPreprocessor()
+    preprocessor.fit(data)
+    processed_data = preprocessor.transform(data)
+    print("Data preprocessing completed.")
+    print("Size of processed data:", processed_data.shape)
+    print("Columns in processed data:", processed_data.columns.tolist())
+    print(processed_data.head())
+
+    trainer = ModelTrainer()
+
+    
+    results = trainer.train_and_evaluate(processed_data, target_column)
+
+    print("Model training and evaluation completed.")
+    print("Results:" , results)

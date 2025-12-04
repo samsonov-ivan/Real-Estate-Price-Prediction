cae667c301308f808aeb317a584917c0c7e1bac8
ivan.samsonov <samsonov.ivan@phystech.edu>
2025-12-04 05:14:09 +0300

    fixed

--- main.py
+++ main.py
@@ -1,23 +1,22 @@
 import pandas as pd
 import numpy as np
 import kagglehub
+from typing import Optional
 from src.preprocessor import CustomPreprocessor
 from src.trainer import ModelTrainer
 
 
-def load_data(file_path = None):
-    """
-    Load dataset from a CSV file.
-    """
+def load_data(file_path: Optional[str] = None) -> pd.DataFrame:
+    """Load dataset from a CSV file and return a DataFrame."""
 
     if file_path is None:
         raise ValueError("No file path provided for dataset.")
-    
+
     return pd.read_csv(file_path)
 
 
 def main():
-    data = load_data("data/raw.csv")
+    data = load_data("data/raw_sample.csv").sample(n=10000, random_state=42)
     print(data.head(5))
     
     target_column = 'price'
@@ -26,7 +25,7 @@ def main():
 
     rename_dict = {'geo_lat': 'latitude', 'geo_lon': 'longitude'}
     data = data.rename(columns=rename_dict)
-
+    
     data = preprocessor.clean_data(data)
     preprocessor.fit(data)
     processed_data = preprocessor.transform(data)
@@ -42,6 +41,8 @@ def main():
 
     print("Model training and evaluation completed.")
     print("Results:" , results)
+    
+    trainer.generate_report(output_dir='reports')
 
 if __name__ == "__main__":
     main()

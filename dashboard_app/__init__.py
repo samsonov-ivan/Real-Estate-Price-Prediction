dccf4cebd8bae4b571143ab269999b02d844c6c6
ivan.samsonov <samsonov.ivan@phystech.edu>
2025-12-12 20:35:46 +0300

    create dashboard module

---
+++ dashboard_app/__init__.py
@@ -0,0 +1,7 @@
+"""
+Dashboard Application Package.
+
+This package contains the modularized Dash application components,
+including state management, UI components, layout definitions,
+and callback logic.
+"""
